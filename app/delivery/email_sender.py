a0460c3af386e61c58a1777ac9f93303b9d76ff4
Masud Ibn Musa <masudibnmusa10@gmail.com>
2026-09-22 23:50:14 +0600

    Adding app/delivery/

---
+++ app/delivery/email_sender.py
@@ -0,0 +1,8 @@
+"""
+Optional: sends the formatted summary via email to attendees.
+"""
+
+
+def send_email(to_addresses: list[str], subject: str, body: str):
+    # TODO: implement via SMTP or an email API (SendGrid, SES, etc.)
+    raise NotImplementedError("Email delivery not yet implemented.")
